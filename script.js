ebb42dc17867045b14ddfac2152665edabf74845
Hiksel <aksel.g@outlook.fr>
2026-04-20 20:01:39 +0200

    Morse

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 function afficherMessage() {
   document.getElementById("confirmation").textContent = "Votre message a bien été envoyé.";
 }
+
+function ajouterSymbole(symbole) {
+  document.getElementById("champ-morse").value += symbole;
+}
