ff0c4354cf4ffff4dee894ec475ff92173d05ea5
Hiksel <aksel.g@outlook.fr>
2026-04-23 17:39:23 +0200

    Morse

--- script.js
+++ script.js
@@ -2,6 +2,13 @@ function afficherMessage() {
   document.getElementById("confirmation").textContent = "Votre message a bien été envoyé.";
 }
 
-function ajouterSymbole(symbole) {
-  document.getElementById("champ-morse").value += symbole;
+function insertMorse(c) {
+  const ta = document.getElementById('champ-morse');
+  const start = ta.selectionStart;
+  const end = ta.selectionEnd;
+  const val = ta.value;
+  ta.value = val.slice(0, start) + c + val.slice(end);
+  const pos = start + c.length;
+  ta.selectionStart = ta.selectionEnd = pos;
+  ta.focus();
 }
